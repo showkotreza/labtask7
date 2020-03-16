8a0f76d94ccb8a5fbe2532f6644d13751a0ca545
showkotreza <57698416+showkotreza@users.noreply.github.com>
2020-03-16 12:26:53 +0600

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<img src="Company.PNG" alt="company logo" height="70" weight="70">
+<p align="right"><a href="index.php">Home |</a>
+<a href="login.php">Log in |</a>
+<a href="registration.php">Registration |</a></p>
+<hr>
+
+<p> Account </p>
+<hr>
+<p> <a href="index.php">Dashboard</a></p>
+<a href="login.php"> View profile</a></p>
+<a href="registration.php">Edit profile</a></p>
+<a href="registration.php">Change profile picture</a></p>
+<a href="registration.php">Change Password</a></p>
+<a href="registration.php">Logout</a></p>
+
+
+
+
+
+
+
+<hr>
+</body>
+</html>
